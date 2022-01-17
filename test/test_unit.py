130ccc7c54272901f3ecd8e5b50d3324b95a4fe9
tommila <14302+tommila@users.noreply.gits-15.sys.kth.se>
2022-01-17 17:28:57 +0100

    added xml reports

--- test/test_unit.py
+++ test/test_unit.py
@@ -6,4 +6,5 @@ class Tests(unittest.TestCase):
         self.assertTrue(True, "hello")
         
 if __name__ == '__main__':
-    unittest.main()
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
